6a526d833ad93c3288c71009f634c91e230961a9
xuri02 <t0bl4s.durst@gmail.com>
2019-11-16 19:45:44 +0100

    finished

--- toolkit/Program.cs
+++ toolkit/Program.cs
@@ -4,6 +4,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Xml.Serialization;
@@ -59,7 +60,7 @@ namespace toolkit {
             Console.WriteLine(msbuild);
             if (!File.Exists(msbuild)) {
                 MessageBox.Show("Pleas Locate MSBuild.exe\nHint:\nC:\\Windows\\WinSxS\\x86_msbuild_[***]4.0.15788.0_none_[***]\\MSBuild.exe\n" + firstFileName, "Locate MSBuild.exe", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
-               
+
                 o.Title = "Pleas Locate MSBuild.exe";
                 o.Filter = "Executable *.exe|*.exe|All Files *.*|*.*";
                 o.InitialDirectory = "C:\\Windows\\WinSxS\\" + firstFileName;
@@ -90,63 +91,72 @@ namespace toolkit {
                 px.WaitForExit();
             }
 
+            if (MessageBox.Show("You Can Custom The Icon!", "Custom ?", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
+                var of = new OpenFileDialog();
+                of.Filter = "Icon *.ico|*.ico";
+                if (of.ShowDialog() == DialogResult.Yes) {
+                    File.Copy(of.FileName, s7Out + "\\5.ico", true);
+                    MessageBox.Show("Copyed Icon");
+                }
+            }
 
             Process pxs = new Process();
             var grgeses =  "/c @echo off && echo !!!!!!!! start  build !!!!!!!! && timeout 2 && "+ o.FileName+ " "+ s7Out + "\\unPack.csproj" + " && echo !!!!!!!! Finished !!!!!!!! && pause";
 
-            Console.WriteLine(grgeses);
+            //Console.WriteLine(grgeses);
             pxs.StartInfo = new ProcessStartInfo("cmd", grgeses);
             pxs.StartInfo.UseShellExecute = false;
             pxs.Start();
             pxs.WaitForExit();
 
-            if(MessageBox.Show("Surcsess ?", "Surcsess", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
+            if (MessageBox.Show("Surcsess ?", "Surcsess", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == DialogResult.Yes) {
                 custom = s7Out + "\\bin\\Debug\\assemblyname.exe";
                 MessageBox.Show("Custom Exe:\n" + custom);
             }
         }
 
         internal static string pack(List<Item> _ls, string arch) {
-            string state = "";
             try {
                 WRITE(ne_file, unPack);
                 WRITE(ne_file, encoding.GetBytes(IDENTY01));
 
-                state = "Copied DePacker";
+                Console.WriteLine("Copied DePacker");
 
                 string name = Path.GetTempPath() + new Random().Next(0, 100).ToString();
                 var fs = File.Open(name, FileMode.OpenOrCreate);
 
                 var serializer = new XmlSerializer(typeof(List<Item>));
                 serializer.Serialize(fs, _ls);
+                fs.Flush();
                 fs.Close();
+                Thread.Sleep(100);
                 fs = File.OpenRead(name);
                 var buffer = new byte[fs.Length];
                 fs.Read(buffer, 0, (int) fs.Length);
 
                 var xml_64 = Convert.ToBase64String(buffer);
 
-                state = "Informations Generated";
+                Console.WriteLine("Informations Generated");
 
                 WRITE(ne_file, encoding.GetBytes(xml_64));
 
                 WRITE(ne_file, encoding.GetBytes(IDENTY02));
 
-                state = "Informations Copied";
+                Console.WriteLine("Informations Copied");
                 var a = File.OpenRead(arch);
                 WRITE(ne_file, READ(a, (int) a.Length));
-                state = "Data Writed";
+                Console.WriteLine("Data Writed");
                 try {
                     a.Close();
                     ne_file.Close();
                     a.Dispose();
                     ne_file.Dispose();
                 } catch { }
-                state = "Files Colsed";
+                Console.WriteLine("Files Colsed");
                 File.Delete(arch);
-                state = "TempFiles Deleated";
+                Console.WriteLine("TempFiles Deleated");
             } catch (Exception e) {
-                return e.Message + "\n" + state;
+                return e.Message;
             }
             return "Erfolgreich erstellt!\n";
         }
